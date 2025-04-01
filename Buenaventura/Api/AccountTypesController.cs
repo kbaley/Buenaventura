9caff32778ba258c6743d55f65e47dc8007b8438
Kyle Baley <kyle@baley.org>
2025-03-31 20:52:48 -0500

    Initial checkin

---
+++ Buenaventura/Api/AccountTypesController.cs
@@ -0,0 +1,16 @@
+﻿using Buenaventura.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buenaventura.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountTypesController : ControllerBase
+{
+
+    [HttpGet]
+    public IEnumerable<string> GetAccountTypes( )
+    {
+        return AccountType.GetAccountTypes();
+    }
+}
