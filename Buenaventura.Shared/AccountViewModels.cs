a097d551154d553962432ad61b6d5fd96b01ddb4
Kyle Baley <kyle@baley.org>
2025-05-13 21:03:13 -0500

    Update packages

--- Buenaventura.Shared/AccountViewModels.cs
+++ Buenaventura.Shared/AccountViewModels.cs
@@ -17,10 +17,10 @@ namespace Buenaventura.Shared
 
     public class AccountWithTransactions
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         public Guid AccountId { get; set; }
         public decimal CurrentBalance { get; set; }
-        public IEnumerable<TransactionForDisplay> Transactions { get; set; }
+        public IEnumerable<TransactionForDisplay> Transactions { get; set; } = [];
     }
 
     public class AccountIdAndBalance
@@ -33,14 +33,14 @@ namespace Buenaventura.Shared
     public class AccountWithBalance
     {
         public Guid AccountId { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         public decimal StartingBalance { get; set; }
         public DateTime StartDate { get; set; }
-        public string Currency { get; set; }
-        public string Vendor { get; set; }
-        public string AccountType { get; set; }
+        public string Currency { get; set; } = "USD";
+        public string Vendor { get; set; } = "";
+        public string AccountType { get; set; } = "";
         public decimal? MortgagePayment { get; set; }
-        public string MortgageType { get; set; }
+        public string MortgageType { get; set; } = "";
         public bool IsHidden { get; set; }
         public int DisplayOrder { get; set; }
         public decimal CurrentBalance { get; set; }
